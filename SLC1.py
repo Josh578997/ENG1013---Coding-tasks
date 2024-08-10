1c4463d171a148ef41aaa791b13f4730187c8b15
Josh578997 <jval0014@student.monash.edu>
2024-08-10 14:35:24 +1000

    vdfdvdv

--- SLC1.py
+++ SLC1.py
@@ -24,38 +24,38 @@ while running:
     licenseSuspension = None
     demeritPoints = None
 
-    if (roadSpeed == 110):
-        if (20 <= speedExeeded <25):
-            penaltyAmount = 370
+    if speedExeeded >0:
+        if roadSpeed == 110:
+            if speedExeeded < 25:
+                if speedExeeded >=20:
+                    penaltyAmount = 370
+                    licenseSuspension = 3
+
+        elif speedExeeded >= 45:
+            penaltyAmount = 925
+            licenseSuspension = 12
+        elif speedExeeded >= 40:
+            penaltyAmount = 786
+            licenseSuspension = 6
+        elif speedExeeded >= 35:
+            penaltyAmount = 693
+            licenseSuspension = 6
+        elif speedExeeded >= 30:
+            penaltyAmount = 601
             licenseSuspension = 3
+        elif speedExeeded >= 25:
+            penaltyAmount = 509
+            licenseSuspension = 3
+        elif speedExeeded >= 10:
+            penaltyAmount = 370
+            demeritPoints = 3
+        elif speedExeeded > 0:
+            penaltyAmount = 231
+            demeritPoints = 1
 
-    elif speedExeeded >= 45:
-        penaltyAmount = 925
-        licenseSuspension = 12
-    elif speedExeeded >= 40:
-        penaltyAmount = 786
-        licenseSuspension = 6
-    elif speedExeeded >= 35:
-        penaltyAmount = 693
-        licenseSuspension = 6
-    elif speedExeeded >= 30:
-        penaltyAmount = 601
-        licenseSuspension = 3
-    elif speedExeeded >= 25:
-        penaltyAmount = 509
-        licenseSuspension = 3
-    elif speedExeeded >= 10:
-        penaltyAmount = 370
-        demeritPoints = 3
-    elif speedExeeded < 10:
-        penaltyAmount = 231
-        demeritPoints = 1
-    elif speedExeeded <=0:
-        print('driver did not exceed the speed limit, no penalty required.')
-
-    if demeritPoints == None:
+    if demeritPoints is None:
         print(f'amount speed limit exceeded by = {speedExeeded:.2f}\npenalty = ${penaltyAmount:.2f}\nlicense suspension = {licenseSuspension} months ')
-    elif licenseSuspension == None:
+    elif licenseSuspension is None:
         print(f'amount speed limit exceeded by = {speedExeeded:.2f}\npenalty = ${penaltyAmount:.2f}\ndemerit points = {licenseSuspension} points ')
 
 
