ed751eb97e63d0072ff75b3828419db7274abe64
Josh578997 <jval0014@student.monash.edu>
2024-08-18 20:28:23 +1000

    cvghncvgnhj

--- SLC2.py
+++ SLC2.py
@@ -1,10 +1,16 @@
-def generate_sequence(startValue,maxValue) -> list[int,float]:
+"""SLC2.py: contains the generate_sequence function"""
+
+__author__ = "Josh Valastro"
+__date_created__ = "18/08/2024"
+__Student_ID__ = "34976078"
+
+def generate_sequence(startValue: int, maxValue: float) -> list[int,float]:
     """
     a function that generates a sequence of numbers when given a start and end value
 
     Args:
-        startValue - the starting value of the list, included in list output
-        maxValue - the limiting value of the list, not included in list output
+        startValue: int - the starting value of the list, included in list output
+        maxValue: float - the final value of the list is greater than this value
     """
     if type(startValue) is not int:
         return []
