8839866dadbecfe34b36d8e4f5f0d41b9d707a94
Josh578997 <jval0014@student.monash.edu>
2024-08-26 20:32:14 +1000

    Update SLC2.py

--- SLC2.py
+++ SLC2.py
@@ -12,25 +12,37 @@ def generate_sequence(startValue, maxValue):
         startValue: int - the starting value of the list, included in list output
         maxValue: float - the final value of the list is greater than this value
     """
+    
     try:
-        startValue = int(startValue)
+        startValue = int(str(startValue))
+        if not isinstance(startValue,int):
+            return []
+    except ValueError:
+        return []
+     
+    try:
+
+        if isinstance(maxValue,int):
+            return []
+        elif not isinstance(maxValue,float):
+            if '.' in maxValue:
+                pass
+            else:
+                return [] 
+        maxValue = float(str(maxValue))
     except ValueError:
         return []
     
-    try:
-        maxValue = float(maxValue)
-    except ValueError:
-        return []
-    
+        
     sequence = []
     sequence.append(round(startValue,0))
     nextValue = startValue
     i = 0
     while nextValue <= maxValue:
         nextValue = round((sequence[i] + 4)**3-2.629952,0)    
-        sequence.append(nextValue)
+        sequence.append(int(nextValue))
         i+=1
     return sequence
 
-lst = generate_sequence('3',99999.9)
+lst = generate_sequence(3,99999)
 print(lst)
