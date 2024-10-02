d14804f9267cbe6574eecc1967d75ea81f6c78b0
Josh578997 <jval0014@student.monash.edu>
2024-10-03 09:35:49 +1000

    ff

--- ai_presentation/visualisations.py
+++ ai_presentation/visualisations.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('ai_presentation.data.csv')
 print(f'{data.info()} \n{data.describe()}\n{data.head}\n{data.isnull().sum()}')
 
