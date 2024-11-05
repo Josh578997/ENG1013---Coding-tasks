2c838cf277db5fc1e3831431dcfd26d6292f1626
Josh578997 <jval0014@student.monash.edu>
2024-11-05 11:11:06 +1100

    gg

---
+++ Exam.py
@@ -0,0 +1,17 @@
+import math, matplotlib
+"""
+
+A document containing exam coding solutions
+Date created: 5/11/2024
+Date last modified: 5/11/2024
+Creator: Josh Valastro
+
+"""
+
+###### Question 1 ######
+
+###### Question 2 ######
+
+###### Question 3 ######
+
+###### Question 4 ######
